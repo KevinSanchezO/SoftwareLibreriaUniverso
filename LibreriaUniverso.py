55bf96f84dadc6b7272e44ca6d1aa9cd6198622b
Kevin Sánchez Obando <kesanchezo@gmail.com>
2023-03-12 20:56:03 -0600

    GUI improvement
    Created a more stablished structure to work with the GUI

--- LibreriaUniverso.py
+++ LibreriaUniverso.py
@@ -1,22 +1,90 @@
+import tkinter as tk
 import customtkinter as ctk
 import os
 from PIL import Image
 
-root = ctk.CTk()
+class MyApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-root.title("Libreria Universo")
-root.geometry("1040x680")
-root.resizable(False, False)
-root._set_appearance_mode("dark")
+        # Set the geometry of the main window
+        self.geometry("1040x680")
 
-frame_header = ctk.CTkFrame(master = root, width=1040, height=80).place(x=0, y=0)
-image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "images")
-logo_image = ctk.CTkImage(Image.open(os.path.join(image_path, "Logo.png")), size=(276, 48))
+        # Create the container frame
+        self.container = tk.Frame(self, bg="#252525")
+        self.container.pack(fill="both", expand=True)
 
-logo_frame_label = ctk.CTkLabel(frame_header, text="", image=logo_image, compound="left")
-logo_frame_label.grid(row=0, column=0, padx=20, pady=10)
+        # Create the header frame
+        self.header = tk.Frame(self.container, bg="#252525", height=50)
+        self.header.grid(row=0, column=0, sticky="ew")
 
+        # Create the logo label
+        self.image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 
+                                "images")
+        self.logo_image = ctk.CTkImage(Image.open(os.path.join(self.image_path, 
+                                "Logo.png")), size=(276, 48))
 
+        self.logo_frame_label = ctk.CTkLabel(self.header, text="", 
+                                image=self.logo_image, compound="left")
 
+        #self.logo = tk.Label(self.header, text="MyApp", font=("Arial", 16), fg="white", bg="#252525")
+        self.logo_frame_label.grid(row=0, column=0, padx=10, pady=10)
 
-root.mainloop()
+        # Create the frames
+        self.frames = {}
+        for F in (Frame1, Frame2):
+            frame = F(self.container, self)
+            self.frames[F] = frame
+            frame.grid(row=1, column=0, sticky="nsew")
+
+        # Show the first frame
+        self.show_frame(Frame1)
+
+    def show_frame(self, frame_class):
+        frame = self.frames[frame_class]
+        frame.tkraise()
+
+class Frame1(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent, bg="#252525")
+
+        # Add the login widgets to this frame
+        label_username = tk.Label(self, text="Username", 
+                        fg="white", bg="#252525").grid(row=0, 
+                        column=0, padx=10, pady=10, sticky="w")
+        entry_username = tk.Entry(self, bg="white").grid(row=0, 
+                        column=1, padx=10, pady=10, sticky="e")
+        label_password = tk.Label(self, text="Password", 
+                        fg="white", bg="#252525").grid(row=1, 
+                        column=0, padx=10, pady=10, sticky="w")
+        entry_password = tk.Entry(self, show="*", bg="white").grid(row=1, 
+                        column=1, padx=10, pady=10, sticky="e")
+        button_1 = tk.Button(self, text="Login", bg="white", fg="#252525", 
+                    command=lambda: controller.show_frame(Frame2)).grid(row=2,
+                    column=0, columnspan=2, padx=10, pady=10)
+
+class Frame2(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent, bg="#252525")
+
+        # Add the menu buttons to this frame
+        button1 = tk.Button(self, text="Option 1", bg="white", fg="#252525")
+        button2 = tk.Button(self, text="Option 2", bg="white", fg="#252525")
+        button3 = tk.Button(self, text="Option 3", bg="white", fg="#252525")
+        button4 = tk.Button(self, text="Option 4", bg="white", fg="#252525")
+
+        # Arrange the buttons in a single row with a padding of 80 pixels
+        button1.pack(side="left", padx=80, pady=200)
+        button2.pack(side="left", padx=80, pady=200)
+        button3.pack(side="left", padx=80, pady=200)
+        button4.pack(side="left", padx=80, pady=200)
+
+        # Add a back button to return to the login screen
+        back_button = tk.Button(self, text="Back", bg="white", 
+                                fg="#252525", 
+                                command=lambda: controller.show_frame(Frame1))
+        back_button.pack(side="bottom")
+
+if __name__ == "__main__":
+    app = MyApp()
+    app.mainloop()
