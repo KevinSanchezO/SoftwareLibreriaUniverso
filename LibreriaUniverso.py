28ab3fbf380da2ebb110b7db8b783112d6cab70c
Kevin Sánchez Obando <kesanchezo@gmail.com>
2023-04-14 00:17:29 -0600

    More GUI modifications

--- LibreriaUniverso.py
+++ LibreriaUniverso.py
@@ -249,71 +249,91 @@ class RegisterNewProduct(tk.Frame):
     def __init__(self, parent, controller):
         super().__init__(parent, bg="#252525")
 
-        font_title = font.Font(size=18, weight="bold")
-        font_subtitle = font.Font(size=14, weight="bold")
         font_label = font.Font(size=12)
 
-        # Title
-        label_title = tk.Label(self, text="Registrar nuevo producto", font=font_title,
-                               fg="white", bg="#252525")
-        label_title.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky="w")
+        font_frame =  ctk.CTkFont(size=18)
+        title_font_frame = ctk.CTkFont(size=38)
+        subtitle_font = ctk.CTkFont(size=28)
+
+        label_title = ctk.CTkLabel(self, text="Menu de inicio", 
+                            font=title_font_frame).place(x=30, y=30)
 
         # Subtitle
-        label_subtitle = tk.Label(self, text="Filtro de registro", font=font_subtitle,
-                                  fg="white", bg="#252525")
-        label_subtitle.grid(row=1, column=0, columnspan=2, padx=10, pady=5, sticky="w")
+        label_subtitle = ctk.CTkLabel(self, text="Filtro de Registro",
+                             font=subtitle_font).place(x=30, y=80)
 
         # Product Name Entry
-        label_product_name = tk.Label(self, text="Nombre de producto", font=font_label,
-                                      fg="white", bg="#252525")
-        label_product_name.grid(row=2, column=0, padx=10, pady=10, sticky="w")
-        self.entry_product_name = tk.Entry(self, bg="#777777", font=font_label)
+        label_product_name = ctk.CTkLabel(self, text="Nombre de producto",
+                                      font=font_frame).place(x=30+40, y=120+40)
+        self.entry_product_name = ctk.CTkEntry(self, width=500,
+                               height=30,
+                               border_width=2, font=font_frame)
         self.entry_product_name.focus()
-        self.entry_product_name.grid(row=2, column=1, padx=10, pady=10, sticky="e")
+        self.entry_product_name.place(x=240+40,y=120+40)
 
         # Date Entry
-        label_product_date = tk.Label(self, text="Fecha de Vencimiento", font=font_label,
-                                      fg="white", bg="#252525")
-        label_product_date.grid(row=4, column=0, padx=10, pady=10, sticky="w")
-        self.entry_product_date = tk.Entry(self, bg="#777777", font=font_label)
-        self.entry_product_date.grid(row=4, column=1, padx=10, pady=10, sticky="e")
+        label_product_date = ctk.CTkLabel(self, text="Fecha de Vencimiento", 
+                                      font=font_frame).place(x=30+40, y=170+40)
+        self.entry_product_date = ctk.CTkEntry(self, width=500,
+                               height=30,
+                               border_width=2, font=font_frame)
+        self.entry_product_date.focus()
+        self.entry_product_date.place(x=240+40,y=170+40)
 
         # Marca Entry
-        label_product_marca = tk.Label(self, text="Marca", font=font_label,
-                                      fg="white", bg="#252525")
-        label_product_marca.grid(row=5, column=0, padx=10, pady=10, sticky="w")
-        self.entry_product_marca = tk.Entry(self, bg="#777777", font=font_label)
-        self.entry_product_marca.grid(row=5, column=1, padx=10, pady=10, sticky="e")
+        label_product_marca = ctk.CTkLabel(self, text="Marca", 
+                                           font=font_frame).place(x=80+40,y=220+40)
+        self.entry_product_marca = ctk.CTkEntry(self, width=500,
+                               height=30,
+                               border_width=2, font=font_frame)
+        self.entry_product_marca.place(x=240+40,y=220+40)
 
         # Initial Quantity Entry
-        label_initial_quantity = tk.Label(self, text="Cantidad inicial", font=font_label,
-                                           fg="white", bg="#252525")
-        label_initial_quantity.grid(row=6, column=0, padx=10, pady=10, sticky="w")
-        self.entry_initial_quantity = tk.Entry(self, bg="#777777", font=font_label)
-        self.entry_initial_quantity.grid(row=6, column=1, padx=10, pady=10, sticky="e")
+        label_initial_quantity = ctk.CTkLabel(self, text="Cantidad inicial", 
+                                          font=font_frame).place(x=45+40, y=270+40)
+        self.entry_initial_quantity = ctk.CTkEntry(self, width=500,
+                               height=30,
+                               border_width=2, font=font_frame)
+        self.entry_initial_quantity.focus()
+        self.entry_initial_quantity.place(x=240+40,y=270+40)
 
         # Initial Quantity Entry
-        label_initial_price = tk.Label(self, text="Precio por unidad", font=font_label,
-                                           fg="white", bg="#252525")
-        label_initial_price.grid(row=7, column=0, padx=10, pady=10, sticky="w")
-        self.entry_initial_price = tk.Entry(self, bg="#777777", font=font_label)
-        self.entry_initial_price.grid(row=7, column=1, padx=10, pady=10, sticky="e")
+        label_initial_price = ctk.CTkLabel(self, text="Precio por unidad", 
+                                           font=font_frame).place(x=40+40,y=320+40)
+        self.entry_initial_price = ctk.CTkEntry(self, width=500,
+                               height=30,
+                               border_width=2, font=font_frame)
+        self.entry_initial_price.focus()
+        self.entry_initial_price.place(x=240+40, y=320+40)
 
         # Save Button
-        button_save = tk.Button(self, text="Guardar", font=font_label, bg="green", fg="white",
-                                command=lambda:controller.verificationRegisterProduct(self.entry_product_name,self.entry_product_date,self.entry_product_marca,
-                                                                                      self.entry_initial_quantity,self.entry_initial_price))
-        button_save.grid(row=8, column=0, padx=10, pady=10, sticky="w")
+        button_save = ctk.CTkButton(self, text="Guardar", 
+                                    font=font_frame, 
+                                    width=180,
+                                    height=40,
+            command=lambda:controller.verificationRegisterProduct(self.entry_product_name,
+                            self.entry_product_date,self.entry_product_marca,
+                            self.entry_initial_quantity,self.entry_initial_price))
+        button_save.place(x=220,y=440)
 
         # Clean Button
-        button_clean = tk.Button(self, text="Limpiar", font=font_label, bg="gray", fg="#FFFFFF",
-                                 command=self.clean_entries)
-        button_clean.grid(row=8, column=1, padx=10, pady=10, sticky="e")
+        button_clean = ctk.CTkButton(self, text="Limpiar", 
+                                    font=font_frame,
+                                    width=180,
+                                    height=40,
+                                    command=self.clean_entries)
+        button_clean.place(x=620,y=440)
 
-        # Clean Button
-        button_volver = tk.Button(self, text="Volver", font=font_label, bg="#D61C1C", fg="white",
-                                 command=lambda: controller.show_frame(InventoryManagementMenu))
-        button_volver.grid(row=8, column=2, padx=10, pady=10, sticky="e")
+        # Back Button
+        back_button = ctk.CTkButton(self, 
+                                    text="Salir", 
+                                    font=font_frame,
+                                    fg_color="#D61C1C",
+                                    hover_color="#9E1818",
+                                    width=180,
+                                    height=40,
+                                    command=lambda: controller.show_frame(InventoryManagementMenu))
+        back_button.place(x=780, y=80)
 
     def save_product(self):
         # Implement code to save the product here
